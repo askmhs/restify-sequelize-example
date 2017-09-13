c44f7fa383d9addc1bbc15f11e1484ea6158f6fa
askmhs <muhammadharits@icloud.com>
2017-09-13 21:36:15 +0700

    - Index starter file

---
+++ index.js
@@ -0,0 +1,20 @@
+import restify from 'restify';
+
+const server = restify.createServer({
+    name: 'restify-sequelize-example'
+});
+
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+restify.CORS.ALLOW_HEADERS.push('content-type');
+restify.CORS.ALLOW_HEADERS.push('accesskey');
+restify.CORS.ALLOW_HEADERS.push('secretkey');
+server.pre(restify.CORS({'origins': ['*']}));
+
+require('./src/Http/Routers/User')(server);
+
+server.listen(8000);
+
+module.exports = server;
